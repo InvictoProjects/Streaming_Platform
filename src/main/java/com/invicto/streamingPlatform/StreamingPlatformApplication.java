150c5628ba8ca6d6733f837e2e4ff48cfd7ac584
vladlinu <vladholik84@gmail.com>
2021-02-24 19:29:22 +0200

    Creating repository

---
+++ src/main/java/com/invicto/streamingPlatform/StreamingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.invicto.streamingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingPlatformApplication.class, args);
+	}
+
+}
