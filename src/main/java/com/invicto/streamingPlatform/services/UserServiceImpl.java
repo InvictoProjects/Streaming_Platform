11a3366c475d60c7da3fcf18e84405548f7e1625
Proxima-C <tretiak2002v@gmail.com>
2021-03-02 15:59:37 +0200

    Create UserServiceImpl.java

---
+++ src/main/java/com/invicto/streamingPlatform/services/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package com.invicto.streamingPlatform.services;
+
+import com.invicto.streamingPlatform.persistence.model.User;
+import com.invicto.streamingPlatform.storage.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private final UserRepository userRepository;
+
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public void createUser(User user) {
+		if (userRepository.existsById(user.getId())) {
+
+			// Throw userAlreadyExists exception
+
+		}
+		userRepository.save(user);
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		if (!userRepository.existsById(user.getId())) {
+
+			// Throw userIsNotExist exception
+
+		}
+		userRepository.delete(user);
+	}
+
+	@Override
+	public void updateUser(User user) {
+		if (!userRepository.existsById(user.getId())) {
+
+			// Throw userIsNotExist exception
+
+		}
+		userRepository.update(user);
+	}
+
+	@Override
+	public List<User> findByLogin(String login) {
+		List<User> users = userRepository.findByLogin(login);
+		if (users == null) {
+
+			// Throw userIsNotExist exception
+
+		}
+		return users;
+	}
+
+	@Override
+	public User findById(Long id) {
+		User user = userRepository.findById(id);
+		if (user == null) {
+
+			// Throw userIsNotExist exception
+
+		}
+		return user;
+	}
+}
