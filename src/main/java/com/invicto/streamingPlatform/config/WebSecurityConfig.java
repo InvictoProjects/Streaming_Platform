318270e5e8ba551d0b7b6aa4086a3f0f8f6c4739
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-03-03 10:58:51 +0200

    Create CustomAuthenticationProvider

--- src/main/java/com/invicto/streamingPlatform/config/WebSecurityConfig.java
+++ src/main/java/com/invicto/streamingPlatform/config/WebSecurityConfig.java
@@ -1,11 +1,21 @@
 package com.invicto.streamingPlatform.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(customAuthenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
--- src/main/java/com/invicto/streamingPlatform/web/controller/UserController.java
+++ src/main/java/com/invicto/streamingPlatform/web/controller/UserController.java
@@ -1,20 +1,11 @@
 package com.invicto.streamingPlatform.web.controller;
 
-import com.invicto.streamingPlatform.persistence.model.User;
-import com.invicto.streamingPlatform.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
 public class UserController {
-    @Autowired
-    public UserService userService;
 
     @GetMapping("/login")
     public String viewLoginPage() {
@@ -22,20 +13,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
-        Pattern emailPattern = Pattern.compile("A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = emailPattern.matcher(username);
-        User user;
-        if (matcher.find()) {
-            user = userService.findByEmailAddress(username);
-        } else {
-            user = userService.findByLogin(username);
-        }
-        if (user != null) {
-            return "redirect:/login?error";
-        } else {
-            // TODO: authorize user
-            return "redirect:/";
-        }
+    public String login() {
+        return "redirect:/";
     }
 }
