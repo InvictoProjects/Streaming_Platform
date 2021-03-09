12871e0162a651a60337b7726960f1d586516085
Proxima-C <proxima2002v@gmail.com>
2021-03-09 20:45:39 +0200

    Update UserService.java

--- src/main/java/com/invicto/streamingPlatform/services/UserService.java
+++ src/main/java/com/invicto/streamingPlatform/services/UserService.java
@@ -2,14 +2,12 @@ package com.invicto.streamingPlatform.services;
 
 import com.invicto.streamingPlatform.persistence.model.User;
 
-import java.util.List;
-
 public interface UserService {
 
 	public void createUser(User user);
 	public void deleteUser(User user);
 	public void updateUser(User user);
-	public List<User> findByLogin(String login);
+	public User findByLogin(String login);
 	public User findByEmailAddress(String emailAddress);
 	public User findById(Long id);
 }
