39d0823f99e8d018c26fbce76cd202a3d6aabc47
Koroliuk <54640416+Koroliuk@users.noreply.github.com>
2021-03-22 21:38:26 +0200

    Update UserServiceImpl.java

--- src/main/java/com/invicto/streaming_platform/services/impl/UserServiceImpl.java
+++ src/main/java/com/invicto/streaming_platform/services/impl/UserServiceImpl.java
@@ -14,7 +14,8 @@ import java.util.regex.Pattern;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
-
+	
+	@Autowired
 	public UserServiceImpl(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
