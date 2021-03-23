b9e6d20035aa198e6da3569a8e6e0fe251b9698f
Vladyslav <proxima2002v@gmail.com>
2021-03-23 06:26:38 +0200

    Update UserServiceImpl.java

--- src/main/java/com/invicto/streaming_platform/services/impl/UserServiceImpl.java
+++ src/main/java/com/invicto/streaming_platform/services/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
