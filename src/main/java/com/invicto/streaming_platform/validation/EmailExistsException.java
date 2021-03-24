39dea0c1c3449dd68ff1ff9c79f4eb20fabd8468
Vlad Golik <moyfolder@gmail.com>
2021-03-24 16:24:44 +0200

    Deleted EmailExistsException.java

--- src/main/java/com/invicto/streaming_platform/validation/EmailExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.invicto.streaming_platform.validation;
-
-public class EmailExistsException extends Throwable {
-    public EmailExistsException(final String message) {
-        super(message);
-    }
-}
