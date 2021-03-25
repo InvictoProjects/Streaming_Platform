9d82cce1951c8ab79477c9d2642266498bd56217
Vladyslav <proxima2002v@gmail.com>
2021-03-25 20:08:15 +0200

    Add Video Service

---
+++ src/main/java/com/invicto/streaming_platform/services/impl/VideoServiceImpl.java
@@ -0,0 +1,68 @@
+package com.invicto.streaming_platform.services.impl;
+
+import com.invicto.streaming_platform.persistence.model.Video;
+import com.invicto.streaming_platform.persistence.repository.VideoRepository;
+import com.invicto.streaming_platform.services.VideoService;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class VideoServiceImpl implements VideoService {
+
+    private final VideoRepository videoRepository;
+
+    public VideoServiceImpl(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+
+    @Override
+    public Video createVideo(Video video) {
+        if (video.getId() == null) {
+            throw new IllegalArgumentException("Video id can not be null");
+        } else if (videoRepository.existsById(video.getId())) {
+            throw new EntityExistsException("Video with id " + video.getId() + " is already exists");
+        }
+        return videoRepository.save(video);
+    }
+
+    @Override
+    public void deleteVideo(Video video) {
+        if (video.getId() == null) {
+            throw new IllegalArgumentException("Video id can not be null");
+        } else if (!videoRepository.existsById(video.getId())) {
+            throw new EntityNotFoundException("Video with id " + video.getId() + " does not exist");
+        }
+        videoRepository.delete(video);
+    }
+
+    @Override
+    public Video updateVideo(Video video) {
+        if (video.getId() == null) {
+            throw new IllegalArgumentException("Video id can not be null");
+        } else if (!videoRepository.existsById(video.getId())) {
+            throw new EntityNotFoundException("Video with id " + video.getId() + " does not exist");
+        }
+        return videoRepository.save(video);
+    }
+
+    @Override
+    public List<Video> findAll() {
+        List<Video> videos = new ArrayList<>();
+        videoRepository.findAll().forEach(videos::add);
+        return videos;
+    }
+
+    @Override
+    public Optional<Video> findById(Long id) {
+        return videoRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Video> findByTitle(String title) {
+        return videoRepository.findByTitle(title);
+    }
+}
