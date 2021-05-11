7df7ae90889727df9ff04e2c2055908e2293f65b
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-05-11 16:17:57 +0300

    Update video.js and add video.css

--- src/main/resources/static/js/video.js
+++ src/main/resources/static/js/video.js
@@ -1,11 +1,32 @@
+'use strict'
 const stringUrl = window.location.href;
-const idx = stringUrl.lastIndexOf('?');
-const path = stringUrl.substring(0, idx);
+const idx1 = stringUrl.lastIndexOf('?');
+const path = stringUrl.substring(0, idx1);
 const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
 
 const video = document.getElementById("video");
 const source = document.createElement("source");
+const span = document.getElementById("detailsInfo")
+
+const request = new XMLHttpRequest;
+const text = span.textContent;
+const idx2 = text.indexOf(" ");
+let views = parseInt(text.substring(0, idx2));
+const data = text.substring(idx2, text.length);
+
+let flag = true;
+video.addEventListener("play", () => {
+    if (flag) {
+        setTimeout(() => {
+            request.open("POST", "/video?id=" + id);
+            request.send();
+            views++;
+            document.getElementById("detailsInfo").textContent = views + data;
+            flag = false;
+        }, 3000);
+    }
+});
 
 source.setAttribute('src', path+"/stream?id="+id);
 source.setAttribute('type', 'video/mp4');
